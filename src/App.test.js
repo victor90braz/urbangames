09e935e994db7874d9ee169b133c37334bbbcbf9
victor90braz <victor.90braz@gmail.com>
2022-09-10 01:09:31 +0200

    Eliminated unnecessary files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Hola Mondo link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hola Mondo/i);
   expect(linkElement).toBeInTheDocument();
 });
+
