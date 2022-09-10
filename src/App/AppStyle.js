9b80d61ffd2688fe224f814f93eea87262a7f304
victor90braz <victor.90braz@gmail.com>
2022-09-10 03:36:27 +0200

    Selected files in package json to ignored while jest run

--- src/App/AppStyle.js
+++ src/App/AppStyle.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const AppStyle = styled.div`
   text-align: center;
   height: 100vh;
-  border: 1px solid red;
+  border: 1px dotted red;
 `;
 
 export default AppStyle;
