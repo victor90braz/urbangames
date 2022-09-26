cddfc525f13db8daa406f4927cae16b3fef68a21
victor90braz <victor.90braz@gmail.com>
2022-09-26 16:00:57 +0200

    Applied style in the container-image

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,8 +3,8 @@ import HomePageStyle from "./HomePageStyle";
 function HomePage() {
   return (
     <HomePageStyle>
-      <div className="hero-image">
-        <div className="hero-text">
+      <div className="container-image">
+        <div className="container-text">
           <h2>Play football</h2>
           <h3>Anytime, anywhere.</h3>
           <button type="button" disabled>
--- src/pages/HomePage/HomePageStyle.js
+++ src/pages/HomePage/HomePageStyle.js
@@ -4,7 +4,7 @@ const HomePageStyle = styled.div`
   h2 {
     font-size: 50px;
   }
-  .hero-image {
+  .container-image {
     background-image: url("/images/soccer-field.jpg");
     background-color: #cccccc;
     height: 500px;
@@ -14,13 +14,22 @@ const HomePageStyle = styled.div`
     position: relative;
   }
 
-  .hero-text {
+  .container-text {
     text-align: center;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     color: white;
+  }
+
+  button {
+    padding: 15px;
+    background-color: #ffcb00;
+    border-radius: 5px;
+    border: none;
+    font-weight: bold;
+    color: black;
   }
 `;
 
