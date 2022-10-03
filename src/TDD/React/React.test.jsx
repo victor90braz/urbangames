1d7376fe5aa4a74c02368836bc9e5480366da786
victor90braz <victor.90braz@gmail.com>
2022-10-03 12:51:02 +0200

    TDD of a calculator

---
+++ src/TDD/React/React.test.jsx
@@ -0,0 +1,48 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import { afterEach, describe, it, expect } from "vitest";
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const rows = [["7", "8", "9"], ["4", "5", "6"], ["1", "2", "3"], ["0"]];
+
+function Calculator() {
+  return (
+    <section>
+      <h1>Calculator</h1>
+      <div role="grid">
+        {rows.map((row) => (
+          <div key={row} role="row">
+            {row.map((number) => (
+              <span key={number}>{number}</span>
+            ))}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+describe("Calculator", () => {
+  afterEach(cleanup);
+  it("should render", () => {
+    render(<Calculator />);
+  });
+
+  it("should render a title", () => {
+    render(<Calculator />);
+
+    screen.getByText("Calculator");
+  });
+
+  it("should render numbers", () => {
+    render(<Calculator />);
+
+    numbers.map((number) => screen.getByText(number));
+  });
+
+  it("should render 4 rows", () => {
+    render(<Calculator />);
+
+    const expectedRows = screen.getAllByRole("row");
+    expect(expectedRows).toHaveLength(4);
+  });
+});
