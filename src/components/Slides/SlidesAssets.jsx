7de091aa51353980bd8cab6a4586551bd040042a
victor90braz <victor.90braz@gmail.com>
2022-10-08 13:06:48 +0200

    Added mediaqueries to Slides component

--- src/components/Slides/SlidesAssets.jsx
+++ src/components/Slides/SlidesAssets.jsx
@@ -3,8 +3,7 @@ const SlidesImages = [
     image: "messi-cr7.jpg",
     alt: "Messi vs CR7",
     title: "Frank Lampard's comments on Messi vs CR7",
-    description:
-      "Former Chelsea midfielder and manager Frank Lampard echoed Lineker's comments, saying: 'I'm going to go with Messi on this on pure, natural talent but what Ronaldo has done has been absolutely incredible.'",
+    description: "I'm going to go with Messi on this on pure, natural talent.",
     id: 1,
   },
   {
@@ -12,7 +11,7 @@ const SlidesImages = [
     alt: "Messi vs CR7",
     title: "Ibrahimovic's comments on Messi",
     description:
-      "Sometimes he did things that I never saw anyone else do. When we’d be winning the game, we’d start to enjoy ourselves and he’d bring it all out.",
+      "When we’d be winning the game, we’d start to enjoy ourselves and he’d bring it all out.",
     id: 2,
   },
   {
@@ -20,7 +19,7 @@ const SlidesImages = [
     alt: "Messi vs CR7",
     title: "Sergio Ramos's comments on CR7",
     description:
-      "The best player in history? CR7. Because when he was at his best nobody could compare. He always had a smile on his face as well. He had skill, scored fantastic goals and put magic into the game.",
+      "The best player in history? CR7. He had skill, scored fantastic goals and put magic into the game.",
     id: 3,
   },
 ];
